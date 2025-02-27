1576df908299c0bdcdaac9f3f049c4adac4ad8a9
ivakhramov <ivan.vakhramov@outlook.com>
2025-02-27 22:56:00 +0300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import name.remal.gradle_plugins.sonarlint.SonarLintExtension
 import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
 import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
 
@@ -6,6 +7,8 @@ plugins {
     idea
     id("io.spring.dependency-management")
     id("org.springframework.boot") apply false
+    id("name.remal.sonarlint")
+    id("com.diffplug.spotless")
 }
 
 idea {
@@ -54,6 +57,20 @@ subprojects {
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
         options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+    }
+
+    apply<name.remal.gradle_plugins.sonarlint.SonarLintPlugin>()
+    configure<SonarLintExtension> {
+        nodeJs {
+            detectNodeJs = false
+            logNodeJsNotFound = false
+        }
+    }
+    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        java {
+            palantirJavaFormat("2.55.0")
+        }
     }
 }
 
--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -3,9 +3,9 @@
  */
 package ru.otus;
 
-
 import com.google.common.base.Joiner;
 
+@SuppressWarnings("java:S106")
 public class HelloOtus {
     public static void main(String... args) {
         String[] words = {"Hello", "Otus"};
