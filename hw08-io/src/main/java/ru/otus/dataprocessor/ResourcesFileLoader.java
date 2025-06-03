9156e055854204da0c9ea95bbf74224eb3b1199f
ivakhramov <ivan.vakhramov@outlook.com>
2025-06-04 02:48:12 +0300

    ДР к лекции 16 готова

---
+++ hw08-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -0,0 +1,30 @@
+package ru.otus.dataprocessor;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import ru.otus.model.Measurement;
+
+public class ResourcesFileLoader implements Loader {
+    private final String fileName;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public List<Measurement> load() {
+        // читает файл, парсит и возвращает результат
+        try (InputStream is = ResourcesFileLoader.class.getClassLoader().getResourceAsStream(fileName)) {
+            if (is == null) {
+                throw new FileProcessException("File not found in resources: " + fileName);
+            }
+            return mapper.readValue(is, new TypeReference<List<Measurement>>() {});
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
+    }
+}
